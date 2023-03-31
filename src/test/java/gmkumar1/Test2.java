3726c8457866a26f430305c40d472f729ef35579
mahess14 <gmksvec14@gmail.com>
2023-03-31 20:48:00 +0530

    commit testing

---
+++ src/test/java/gmkumar1/Test2.java
@@ -0,0 +1,11 @@
+package gmkumar1;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		
+			System.out.println("git testing");
+
+	}
+
+}
---
+++ src/test/java/gmkumar1/Test3.java
@@ -0,0 +1,12 @@
+package gmkumar1;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		
+			System.out.println("git testing to local");
+
+	}
+
+}
+
